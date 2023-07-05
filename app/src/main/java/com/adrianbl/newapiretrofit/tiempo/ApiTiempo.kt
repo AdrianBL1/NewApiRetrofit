c0545d7103342d8b248ee5ffeaa3e88261b02ea9
Adrian Basilio López <BL_980713@hotmail.com>
2023-07-05 00:33:24 -0600

    API key Update

--- app/src/main/java/com/adrianbl/newapiretrofit/tiempo/ApiTiempo.kt
+++ app/src/main/java/com/adrianbl/newapiretrofit/tiempo/ApiTiempo.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 
 interface ApiTiempo {
     // Direccion URL
-    @GET("data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1")
+    @GET("data/2.5/forecast?id=524901&appid=64a51a5709c405e548b43f6db7da4c90")
 
     abstract fun getTiempo(): Call<Metereologia>
 }
